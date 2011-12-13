7131da77378b1b17e99562f91c14ad16bf51edac
Eric Noulard <eric.noulard@gmail.com>
2011-12-13 23:53:33 +0000

    Update GPL header

--- src/hla/rti1516e/jlc/BasicHLAfloat32BE.java
+++ src/hla/rti1516e/jlc/BasicHLAfloat32BE.java
@@ -5,6 +5,25 @@ import hla.rti1516e.encoding.DecoderException;
 import hla.rti1516e.encoding.EncoderException;
 import hla.rti1516e.encoding.HLAfloat32BE;
 
+//----------------------------------------------------------------------------
+//CERTI - HLA RunTime Infrastructure
+//Copyright (C) 2011 Eric Noulard
+//
+//This program is free software ; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public License
+//as published by the Free Software Foundation ; either version 2 of
+//the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful, but
+//WITHOUT ANY WARRANTY ; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public
+//License along with this program ; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//----------------------------------------------------------------------------
 public class BasicHLAfloat32BE extends DataElementBase implements HLAfloat32BE {
 
     private float value;
